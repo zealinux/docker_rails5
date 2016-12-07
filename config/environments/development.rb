bc2bc452ffc940b5aa8a0303ace8c21b3584e78c
Mike King <zealinux@gmail.com>
2016-12-07 17:31:06 +0800

    Fix local development environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Web Console whitelist the whole private network
+  config.web_console.whiny_requests = false
 end
